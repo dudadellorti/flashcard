3af0cbf7a55f7fc17d780510c3ab7870977e85ad
dudadellorti <00001069563274sp@al.educacao.sp.gov.br>
2024-11-20 14:17:12 -0300

    arquivos

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Vida',
+    'Quem é o homem mais bonito do mundo?',
+    'Ian meu namorado lindo e perfeito'
+)
+
+criaCartao(
+    'Geografia',
+    'Onde fica o Moraes Salles?',
+    'No bairro do Matadouro'
+)
+
+criaCartao(
+    'Vida',
+    'Quem é minha irmã?',
+    'Mariah'
+)
+
+criaCartao(
+    'Vida',
+    'Quem é meu irmão?',
+    'Dichava Teteu'
+)
